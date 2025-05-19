e913af36092a0f594e3a094832b238c9b83de0c3
Manuel Rivas <rexdaro@gmail.com>
2025-05-19 10:57:19 -0300

    commit 19-05 10:57

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+let nombre = 'Manuel';
+let apellido = 'Rivas';
+let edad = 27;
+let esProgramdor = true;
+// array
+let hobbies = ['comer', 'programar', 'tocar guitarra'];
+//tupla
+let persona = ['tupla', true, 5];
+let producto = ['Leche en polvo La Serenisima 800gr', 8500, false];
+console.log(`hola mi nombre es: ${nombre} ${apellido}, tengo ${edad} años y  es cierto que estudio programacion: ${esProgramdor}`);
+console.log(hobbies[2]);
+console.log(persona[0]);
+console.log(producto);
--- app.ts
+++ app.ts
@@ -1,1 +1,18 @@
-console.log('hola mundo');
+let nombre: string = 'Manuel';
+let apellido: string = 'Rivas';
+
+let edad: number = 27;
+let esProgramdor: boolean = true;
+
+// array
+let hobbies: string[] = ['comer', 'programar', 'tocar guitarra'];
+//tupla
+let persona: [string, boolean, number] = ['tupla', true, 5];
+
+let producto: [string, number, boolean] = ['Leche en polvo La Serenisima 800gr', 8500, false];
+
+console.log(`hola mi nombre es: ${nombre} ${apellido}, tengo ${edad} años y  es cierto que estudio programacion: ${esProgramdor}`);
+console.log(hobbies[2]);
+console.log(persona[0]);
+
+console.log(producto);
